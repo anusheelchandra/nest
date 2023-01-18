e2df771e4cdf858064005c1d17c4cf9e2d3b29bb
Monzione <Ottavio.Monzione@finastra.com>
2023-01-18 17:41:28 +0100

    chore: added pipeline, docker image creation and peanut profile

--- src/main/java/com/nest/nest/config/MinioConfig.java
+++ src/main/java/com/nest/nest/config/MinioConfig.java
@@ -2,6 +2,7 @@ package com.nest.nest.config;
 
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Getter
 @ConfigurationProperties(prefix = "minio")
